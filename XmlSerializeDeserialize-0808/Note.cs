63808cb9b242d213e1c50602e1b004d9ac419fbf
Gusts Linkevics <gusts@gst.lv>
2022-10-17 19:17:43 +0300

    Added custom attributes json

--- XmlSerializeDeserialize-0808/Note.cs
+++ XmlSerializeDeserialize-0808/Note.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
@@ -11,15 +12,19 @@ namespace XmlSerializeDeserialize_0808
     public class Note
     {
         [XmlElement("to")]
+        [JsonPropertyName("to")]
         public string To { get; set; }
         
         [XmlElement("from")]
+        [JsonPropertyName("from")]
         public string From { get; set; }
 
         [XmlElement("heading")]
+        [JsonPropertyName("heading")]
         public string Heading { get; set; }
 
         [XmlElement("body")]
+        [JsonPropertyName("body")]
         public string Body { get; set; }
     }
 }
