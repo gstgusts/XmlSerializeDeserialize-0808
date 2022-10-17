1b02c69793d88a77b0a844791a07278f85d6cc7f
Gusts Linkevics <gusts@gst.lv>
2022-10-17 19:08:16 +0300

    Add project files.

---
+++ XmlSerializeDeserialize-0808/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+namespace XmlSerializeDeserialize_0808
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var note = new Note()
+            {
+                To = "Gusts",
+                From = "Gusts",
+                Heading = "Some news",
+                Body = "ajsdk ljas;kdf jas;kdf as;kdfkdffjkf"
+            };
+
+            XmlSerializer xsSubmit = new XmlSerializer(typeof(Note));
+
+            using (var sww = new StringWriter())
+            {
+                using (XmlWriter writer = XmlWriter.Create(sww))
+                {
+                    xsSubmit.Serialize(writer, note);
+                    Console.WriteLine(sww.ToString());
+                }
+            }
+
+
+
+            using (var stream = System.IO.File.OpenRead("note.xml"))
+            {
+                var serializer = new XmlSerializer(typeof(Note));
+                var note2 = serializer.Deserialize(stream) as Note;
+
+                Console.WriteLine(note2.From);
+            }
+
+            
+        }
+    }
+}
